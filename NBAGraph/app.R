03e83a900a8ebda2d1d742e84fe4248865504806
mrrain02 <saavan.kaneria@gmail.com>
2023-11-22 14:02:37 -0500

    R Shiny Application is Working. Start data cleaning and analysis now.

--- NBAGraph/app.R
+++ NBAGraph/app.R
@@ -1,61 +1,70 @@
-
-library(plotly)
 library(shiny)
-library(grid)
 library(ggplot2)
-library(jpeg)
 
-# Read the CSV file with player data
-all_shots <- read.csv("2022_shot_data_new.csv")
+# Load the shot data
+shot_data <- read.csv("2022_shot_data_new.csv")
 
-# Define UI
+# Define the UI for the application
 ui <- fluidPage(
-  titlePanel("Basketball Shots Explorer"),
+  
+  # Application title
+  titlePanel("NBA Shot Chart Explorer"),
+  
+  # Sidebar with player selection dropdown
   sidebarLayout(
+    
     sidebarPanel(
-      # Dropdown for player selection
-      selectInput("player", "Select Player:", choices = unique(all_shots$full_name)),
-      br(),
-      helpText("Select a player from the dropdown.")
+      selectInput("player_name", "Select Player:", choices = unique(shot_data$full_name))
     ),
+    
+    # Main panel with shot chart
     mainPanel(
-      # Output: Basketball court plot
-      plotlyOutput("basketballCourt")
+      plotOutput("shot_chart")
     )
   )
 )
 
-# Define server
+# Define the server logic for the application
 server <- function(input, output) {
-  # Reactive function to filter data based on player selection
-  selected_player_shots <- reactive({
-    all_shots[all_shots$full_name == input$player, ]
+  
+  # Reactive expression to filter shot data based on selected player
+  filtered_shots <- reactive({
+    player_name <- input$player_name
+    if (player_name == "") {
+      return(shot_data)
+    } else {
+      return(shot_data[shot_data$full_name == player_name, ])
+    }
   })
-  # courtImg.URL <- "https://thedatagame.files.wordpress.com/2016/03/nba_court.jpg"
-  # court <- rasterGrob(readJPEG(getURLContent(courtImg.URL)),
-  #                     width=unit(1,"npc"), height=unit(1,"npc"))
-  # Output: ggplot plot
-  output$ggplotChart <- renderPlot({
-    ggplot(selected_player_shots(), aes(x = LOC_X, y = LOC_Y)) +
-      geom_point(aes(colour = SHOT_ZONE_BASIC, shape = EVENT_TYPE)) +
-      xlim(250, -250) +
-      ylim(-50, 420) +
-      geom_rug(alpha = 0.2) +
-      coord_fixed() +
-      ggtitle(paste("Shot Chart\n", unique(selected_player_shots()$full_name), sep = "")) +
-      theme(line = element_blank(),
-            axis.title.x = element_blank(),
-            axis.title.y = element_blank(),
-            axis.text.x = element_blank(),
-            axis.text.y = element_blank(),
-            legend.title = element_blank(),
-            plot.title = element_text(size = 15, lineheight = 0.9, face = "bold"))
+  
+  # Generate shot chart
+  output$shot_chart <- renderPlot({
+    filtered_shots <- filtered_shots()
+    made_shots <- filtered_shots[filtered_shots$SHOT_MADE_FLAG == 1, ]
+    missed_shots <- filtered_shots[filtered_shots$SHOT_MADE_FLAG == 0, ]
+    
+    total_made <- nrow(made_shots)
+    total_missed <- nrow(missed_shots)
+    total_shots <- total_made + total_missed
+    
+    fga_percentage <- ifelse(total_shots > 0, sprintf("%.1f%%", (total_made / total_shots) * 100), "N/A")
+    
+    court_plot <- ggplot() +
+      geom_point(data = made_shots, aes(x = LOC_X, y = LOC_Y), color = "green", size = 5) +
+      geom_point(data = missed_shots, aes(x = LOC_X, y = LOC_Y), color = "red", size = 5) +
+      coord_fixed(xlim = c(-250, 250), ylim = c(-250, 250)) +
+      scale_x_continuous(breaks = seq(-225, 225, 50), labels = seq(-225, 225, 50)) +
+      scale_y_continuous(breaks = seq(-225, 225, 50), labels = seq(-225, 225, 50)) +
+      theme_bw() +
+      theme(panel.grid = element_blank()) +  # Remove gridlines
+      labs(title = paste0("Shot Chart for Player: ", filtered_shots()$PLAYER_NAME[1]),
+           subtitle = paste("Total Made Shots:", total_made, " | Total Missed Shots:", total_missed,
+                            " | FGA Percentage:", fga_percentage),
+           caption = "Note: Green dots represent made shots, and red dots represent missed shots.")
+    
+    print(court_plot)
   })
 }
-
-
-
-
 
 # Run the application
 shinyApp(ui = ui, server = server)
